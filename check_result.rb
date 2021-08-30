fea3ba5d7d5df14a5e5496a32b8031bd73805835
Juanjo Bazán <jjbazan@gmail.com>
2021-08-30 11:35:55 +0200

    step to check result

---
+++ check_result.rb
@@ -0,0 +1,15 @@
+paper_md_path = ARGV[0].to_s
+
+if paper_md_path.empty?
+  puts "   !! ERROR: The paper path is empty"
+  exit 1
+else
+  paper_pdf_path = File.dirname(paper_md_path)+"/paper.pdf"
+  if File.exist?(paper_pdf_path)
+    puts "PDF file generated: #{paper_pdf_path}"
+    exit 0
+  else
+    puts "   !! ERROR: Failed to generate PDF file"
+    exit 1
+  end
+end
